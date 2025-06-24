b61dbe101e5304957775fe652949b1d4038f492e
alextalijan <acatalijan@icloud.com>
2025-06-24 11:34:58 -0500

    feat: add basic html and styling to the weather form

---
+++ js/getIconSRC.js
@@ -0,0 +1,5 @@
+const getIconSRC = function thatReturnsURLOfWeatherIcon(iconDescription) {
+  return `https://github.com/visualcrossing/WeatherIcons/blob/main/PNG/3rd%20Set%20-%20Color/${iconDescription}`;
+};
+
+export default getIconSRC;
