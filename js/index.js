adcbaba80c3978c5dfdb6805db7f60a205f4af91
alextalijan <acatalijan@icloud.com>
2025-06-24 13:52:05 -0500

    feat: write pubsub logic for code communication

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 import getLocationWeather from './getLocationWeather.js';
+import renderWeather from './renderWeather.js';
+import PubSub from 'PubSub.js';
 
 const searchWeatherBtn = document.querySelector('.search-weather-btn');
 searchWeatherBtn.addEventListener('click', (event) => {
